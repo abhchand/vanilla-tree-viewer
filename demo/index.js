8a5e2a99624e264d1bd7c0805dccd3cc1732479d
abhchand <me@abhchand.me>
2021-11-28 00:55:50 -0500

    Moving demo SVG logo to separate file

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,11 @@
 import './template.scss';
+import Logo from './logo.svg';
+
 import VanillaTreeViewer from '../src/components/VanillaTreeViewer/VanillaTreeViewer';
 
 document.addEventListener('DOMContentLoaded', function() {
+  // Load SVG logo
+  document.querySelector('.logo-link').innerHTML = Logo;
+
   VanillaTreeViewer.renderAll();
 }, false);
